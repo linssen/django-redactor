960a6a140d4bd1f151c48933f0643e6232e5dec0
Wil Linssen <wil@linssen.me>
2012-12-01 15:42:08 +0000

    uses the correct file name

--- redactor/views.py
+++ redactor/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 import json
 
 
@@ -9,15 +10,16 @@ import json
 @require_POST
 @login_required
 def upload(request):
+    images = []
     for f in request.FILES.getlist('file'):
-        destination = open('media/test.png', 'wb+')
+
+        name = 'media/%s' % f.name
+        destination = open(name, 'wb+')
         for chunk in f.chunks():
             destination.write(chunk)
         destination.close()
+        images.append({'filelink': '/%s' % name})
 
     response = {"error": "Just an error"}
-    images = [
-        {'filelink': '/media/test.png'},
-    ]
     return HttpResponse(json.dumps(images), mimetype='application/json')
 
